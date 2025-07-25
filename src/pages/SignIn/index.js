59eb7a76db51cf28368881219aaaef2feb3cbc27
kumarnikhilx <kumarnikhilx007@gmail.com>
2025-07-26 03:13:06 +0530

    made small changes

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -23,11 +23,11 @@ function SignIn() {
             y="0px"
             viewBox="0 0 1921 819.8"
           >
-            {" "}
+          
             <path
               class="st0"
               d="M1921,413.1v406.7H0V0.5h0.4l228.1,598.3c30,74.4,80.8,130.6,152.5,168.6c107.6,57,212.1,40.7,245.7,34.4 c22.4-4.2,54.9-13.1,97.5-26.6L1921,400.5V413.1z"
-            ></path>{" "}
+            ></path>
           </svg>
         </div>
 
@@ -69,7 +69,7 @@ function SignIn() {
               </div>
 
               <p>
-                Not Registered?{" "}
+                Not Registered?
                 <Link to="/singUp" className="border-effect cursor">
                   Sign Up
                 </Link>
